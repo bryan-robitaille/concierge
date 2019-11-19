f3754c42dfb2d1a1fcb596842e631c1179248e1b
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2019-11-19 09:58:33 -0500

    Added environment variable for Redis

--- pleio_account/settings.py
+++ pleio_account/settings.py
@@ -141,14 +141,14 @@ SESSION_ENGINE = 'user_sessions.backends.db'
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://redis:6379/0',
+        'LOCATION': f'redis://{os.environ.get('REDIS_HOST')}:6379/0',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
     },
     'axes_cache': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://redis:6379/1',
+        'LOCATION': f'redis://{os.environ.get('REDIS_HOST')}:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient'
         }
